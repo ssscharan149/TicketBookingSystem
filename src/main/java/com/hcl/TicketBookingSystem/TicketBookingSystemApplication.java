da0818493dafebddf2374d4ef822d237edef487f
achewadakhil <achewadakhil11@gmail.com>
2026-02-18 11:53:05 +0530

    Initial commit

---
+++ src/main/java/com/hcl/TicketBookingSystem/TicketBookingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.TicketBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingSystemApplication.class, args);
+		System.out.println("Server is running succesfully");
+	}
+
+}
